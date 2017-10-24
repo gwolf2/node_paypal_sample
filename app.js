2c28c5ea14c1ce810c7ae34a8c6a20cadb3db525
Gavin Wolf <gavinwolf@Gavins-MacBook-Pro.local>
2017-10-24 09:38:12 -0400

    Create landing page

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const ejs = require('ejs');
+const paypal = require('paypal-rest-sdk');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(3000, () => console.log('Server Started'));
